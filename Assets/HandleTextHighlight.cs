e5ffda6d6dd26d46129926af7dbfce7f6287dbdf
fluffydeer <natiklementova@gmail.com>
2022-03-03 19:44:32 +0100

    branch 4
    -just basic skeleton so far, not much functionality

---
+++ Assets/HandleTextHighlight.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//mozem to premenovat na handletabletext
+public class HandleTextHighlight : MonoBehaviour
+{    private void OnTriggerEnter(Collider other) {
+        if(this.name == "OKButton"){
+            Debug.Log("OKBUTTON");
+        }
+        Debug.Log(this.name + " was entered");
+        Debug.Log(other.name);
+    }
+}
