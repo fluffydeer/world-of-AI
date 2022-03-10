fbe5ef15a7b0b851610f950dccf9ee77a17513ec
fluffydeer <natiklementova@gmail.com>
2022-03-10 10:27:29 +0100

    level 1 fixes #3

--- Assets/BinManager.cs
+++ Assets/BinManager.cs
@@ -2,14 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR.InteractionSystem;
+using TMPro;
 
 public class BinManager : MonoBehaviour
 {
+    [SerializeField] GameObject scorebar;
+    [SerializeField] GameObject slider;
+    [SerializeField] int garbageAmount;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] AudioClip correctAnswerSound;
+    [SerializeField] AudioClip incorrectAnswerSound;
+    private AudioSource playerAudio;
+    private float scorebarWidth;
+    private float initialSliderWidth;
+    private int correctAnswers = 0;
+
     void Start()
     {
-        
+        scorebarWidth = scorebar.transform.localScale.x; //0.7
+        initialSliderWidth = slider.transform.localScale.x;    //0.0875
+        playerAudio = GetComponent<AudioSource>();
     }
-
+ 
     void Update()
     {
         
@@ -20,11 +34,50 @@ public class BinManager : MonoBehaviour
         Debug.Log("other tag: " + other.gameObject.tag);
         Debug.Log("bin: " + gameObject.tag);
         if (other.gameObject.CompareTag(gameObject.tag)){
-            Debug.Log("dobre") ;
+            Debug.Log("dobre");
+            CorrectAnswer();
         }
         else
         {
             Debug.Log("zle");
+            Transform transform = other.gameObject.transform;
+            transform.position = new Vector3(0,0,+5f);
+            playerAudio.PlayOneShot(incorrectAnswerSound, 1.0f);
+        }
+    }
+
+
+    //implementovat aj OnTriggerExit? 
+    private void CorrectAnswer(){
+        correctAnswers++;
+        SetSliderWidth();
+        SetSliderPosition();
+        playerAudio.PlayOneShot(correctAnswerSound, 1.0f);
+        scoreText.text = "heh";
+        scoreText.text = $"\n \nRoztriedené\n \n \n {correctAnswers}/8";
+
+        //demobilizovat danu vec aby zotrvala v tom bine -
+        //freeze x,y,z? 
+        //potom by netrebalo riesit OnTriggerExit
+        IsDone();
+    }
+
+    private void SetSliderWidth(){
+        float x = correctAnswers * initialSliderWidth;
+        Vector3 scale = slider.transform.localScale;
+        slider.transform.localScale = new Vector3(x, scale.y, scale.z);
+    }
+
+    private void SetSliderPosition(){
+        float x = scorebarWidth/2 -  (initialSliderWidth * correctAnswers)/2;
+        Vector3 position = slider.transform.position;
+        slider.transform.position = new Vector3(x, position.y, position.z);
+    }
+
+    private void IsDone(){
+        if(correctAnswers == garbageAmount){
+            scoreText.text = "done";
+
         }
     }
 }
--- Assets/Tablet.cs
+++ Assets/Tablet.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //mozem to premenovat na handletabletext
-public class HandleTextHighlight : MonoBehaviour
+public class Tablet : MonoBehaviour
 {    private void OnTriggerEnter(Collider other) {
         if(this.name == "OKButton"){
             Debug.Log("OKBUTTON");
+        }
+        if(gameObject.CompareTag("TabletData")){
+            Debug.Log("TABLETDATA");
         }
         Debug.Log(this.name + " was entered");
         Debug.Log(other.name);
