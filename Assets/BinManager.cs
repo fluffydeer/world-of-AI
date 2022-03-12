ef327331a0985585dcbc4b9a7c78163d9215d72e
fluffydeer <natiklementova@gmail.com>
2022-03-12 15:03:45 +0100

    update

--- Assets/BinManager.cs
+++ Assets/BinManager.cs
@@ -13,6 +13,8 @@ using TMPro;
 //v hre sa da zaradit viac ako 8 objektov do kosov co je blbost
 //bolo by fajn tam mat 10 predmetov na zaradenie
 //otestovat to aby sa objekty zaratali len ak su vo vnutri kosa a nie ak sa dotknu vonka
+//-toto kvoli tomu, ze je to vr sa ani neda moc tomu zabranit, jedine pomocou kodu
+
 //repnut zaciatocnu tabulu na skore potom co 1. objekt je v kosi
 //na konci znicit tuto tabulu po par sekundach a ukazat cestu dalej
 
@@ -42,14 +44,25 @@ public class BinManager : MonoBehaviour
         Debug.Log("bin: " + gameObject.tag);
         if (other.gameObject.CompareTag(gameObject.tag)){
             Debug.Log("dobre");
+            //freeze position
+            //len ci toto zastavi aby interactable fungovalo nadalej
+            Rigidbody rb = other.GetComponent<Rigidbody>(); 
+            rb.constraints = RigidbodyConstraints.FreezePosition;
+            other.GetComponent<Interactable>().enabled = false;
+            other.GetComponent<Throwable>().enabled = false;
             CorrectAnswer();
         }
         else
         {
             Debug.Log("zle");
-            Transform transform = other.gameObject.transform;
-            transform.position = new Vector3(0,0,+5f);
             binAudio.PlayOneShot(incorrectAnswerSound, 1.0f);
+            //find child called spawnpoint
+            //assign this spawnpoint to "new position"
+            Transform spawnPoint = other.transform.Find("SpawnPoint");
+            Debug.Log(spawnPoint);
+            transform.position = spawnPoint.position;
+//            transform.position = new Vector3(0,0,+5f);
+
         }
     }
 
@@ -73,12 +86,14 @@ public class BinManager : MonoBehaviour
         float x = correctAnswers * initialSliderWidth;
         Vector3 scale = slider.transform.localScale;
         slider.transform.localScale = new Vector3(x, scale.y, scale.z);
+        Debug.Log(slider.transform.localScale);
     }
 
     private void SetSliderPosition(){
         float x = scorebarWidth/2 -  (initialSliderWidth * correctAnswers)/2;
         Vector3 position = slider.transform.position;
         slider.transform.position = new Vector3(x, position.y, position.z);
+        Debug.Log(slider.transform.position);
     }
 
     private void IsDone(){
