20add6bfa52409ee272b599f0d3fbdee89a8bc7d
fluffydeer <natiklementova@gmail.com>
2022-04-02 15:07:52 +0200

    folder structure

---
+++ Assets/SortTrainingData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SortTrainingData : MonoBehaviour
+{
+    
+    [SerializeField] 
+    private Image image;
+    
+    [SerializeField] 
+    private List<Sprite> sprites;
+
+    void Start()
+    {
+        SetSprite(6);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SetSprite(int i){
+        image.sprite = sprites[i];
+    }
+}
