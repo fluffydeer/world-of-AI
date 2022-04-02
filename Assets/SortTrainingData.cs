bdb9992f4c2e30ba9f16bf2b72e1c4f3bb7c3178
fluffydeer <natiklementova@gmail.com>
2022-04-02 15:47:15 +0200

    hover button

--- Assets/SortTrainingData.cs
+++ Assets/SortTrainingData.cs
@@ -10,19 +10,32 @@ public class SortTrainingData : MonoBehaviour
     private Image image;
     
     [SerializeField] 
+    //images are sorted in asc order 
     private List<Sprite> sprites;
 
     void Start()
     {
-        SetSprite(6);
-    }
-
-    void Update()
-    {
-        
+        ShowNextSprite();
     }
 
     public void SetSprite(int i){
         image.sprite = sprites[i];
+    }
+
+    public void ShowNextSprite(){
+        if(sprites.Count > 0){
+            int randomIndex = Random.Range(0, sprites.Count);
+            SetSprite(randomIndex);
+            sprites.RemoveAt(randomIndex);
+        }else{
+            TrainingOver();
+        }
+    }
+
+    private void TrainingOver(){
+        Debug.Log("Training is over");
+        //show ui message that its done and he can continue journey
+        //remove triggers from keyboards? so user cant fire shownextsprite all the time
+        //or a variable checking is traning is really over
     }
 }
