310bd4735baab0ea96d96ced721e22f528646947
fluffydeer <natiklementova@gmail.com>
2022-04-19 18:38:31 +0200

    neural network level
    -panel at the second table
    -disc drive to fbx
    -menu portal

--- Assets/Scripts/Neural network/SnapZone.cs
+++ Assets/Scripts/Neural network/SnapZone.cs
@@ -41,30 +41,30 @@ public class SnapZone : MonoBehaviour
         something.SetActive(true);
     }
 
-    private void boze(){
-            CD_in_driver.SetActive(true);
-            NeuralNetworkManager.Instance.PlayCDPlayerClosingSound();
+    private void boze() {
+        CD_in_driver.SetActive(true);
+        NeuralNetworkManager.Instance.PlayCDPlayerClosingSound();
 
-            //asi toto zabija tu korutinu
-            //gameObject.SetActive(false);  
-            /*It's not true, though. I've tried this and for me the coroutines 
-            are stopped as soon as I call SetActive(false) on the MonoBehaviour.
-             EDIT: As @Bunny83 has pointed out, SetActive() disables the 
-             GameObject, which is not the same as disabling the MonoBehaviour. 
-             I still find it very misleading that the documentation never 
-             mentions that disabling a GameObject also stops all coroutines. 
-            They do not continue when you re-enable the object.*/ 
-            //Renderer renderer = GetComponent<MeshRenderer>();
-            Color c = new Color(0,0,0,0);
-            renderer.material.color = c;
+        //asi toto zabija tu korutinu
+        //gameObject.SetActive(false);  
+        /*It's not true, though. I've tried this and for me the coroutines 
+        are stopped as soon as I call SetActive(false) on the MonoBehaviour.
+         EDIT: As @Bunny83 has pointed out, SetActive() disables the 
+         GameObject, which is not the same as disabling the MonoBehaviour. 
+         I still find it very misleading that the documentation never 
+         mentions that disabling a GameObject also stops all coroutines. 
+        They do not continue when you re-enable the object.*/
+        //Renderer renderer = GetComponent<MeshRenderer>();
+        Color c = new Color(0, 0, 0, 0);
+        renderer.material.color = c;
 
-            //animate the cd player
-            Debug.Log("animating");
-            anim.Play(closeCDDrive, 0, 0.0f);
+        //animate the cd player
+        Debug.Log("animating");
+        anim.Play(closeCDDrive, 0, 0.0f);
 
-            StartCoroutine(NeuralNetworkManager.Instance.ShowWithDelay(outputText));
-            electricityParticleSystem.Play();
+        StartCoroutine(NeuralNetworkManager.Instance.ShowWithDelay(outputText));
+        electricityParticleSystem.Play();
 
-            teleportArea.GetComponent<TeleportArea>().locked = false;
+        teleportArea.GetComponent<TeleportArea>().locked = false;
     }
 }
